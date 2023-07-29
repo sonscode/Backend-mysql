314b25faacbf6cc5c2515fa6340b5ae1135122b9
sonscode <ulrichsonna20@gmail.com>
2023-07-29 13:39:04 +0100

    Adding mysql2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const routes = require('./routes');
 const cors = require('cors'); // Import the cors package
 
