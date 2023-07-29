11565086ae1d223a27ca905a30220b4f2b392963
sonscode <ulrichsonna20@gmail.com>
2023-07-29 08:20:10 +0100

    Initial commit

---
+++ routes.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const router = express.Router();
+
+// Middleware to parse JSON data
+router.use(express.json());
+
+// Get all reports
+router.get('/reports', (req, res) => {
+  const pool = req.app.locals.pool; // Access the pool variable from the app locals
+  pool.query('SELECT * FROM reportList', (err, results) => {
+    if (err) {
+      console.error('Error fetching data from the database:', err);
+      res.status(500).json({ error: 'Failed to fetch data from the database' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Get a single report by ID
+router.get('/reports/:id', (req, res) => {
+  const pool = req.app.locals.pool; // Access the pool variable from the app locals
+  const reportId = req.params.id;
+  pool.query('SELECT * FROM reportList WHERE id = ?', [reportId], (err, results) => {
+    if (err) {
+      console.error('Error fetching data from the database:', err);
+      res.status(500).json({ error: 'Failed to fetch data from the database' });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: 'Report not found' });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+});
+
+// POST: Create a new report
+router.post('/reports', (req, res) => {
+    console.log(req.body)
+    const pool = req.app.locals.pool;
+  
+    const fields = [
+        'term', 'name', 'class', 'DOB', 'POB', 'enrollment', 'master', 'situation', 'test1', 'test2',
+        'eng1', 'fre1', 'math1', 'hist1', 'lit1', 'geo1', 'econs1', 'comm1', 'acc1', 'citi1', 'rel1', 'food1',
+        'chem1', 'bio1', 'phy1', 'comp1', 'sport1', 'logic1', 'hb1',
+        'eng2', 'fre2', 'math2', 'hist2', 'lit2', 'geo2', 'econs2', 'comm2', 'acc2', 'citi2', 'rel2', 'food2',
+        'chem2', 'bio2', 'phy2', 'comp2', 'sport2', 'logic2', 'hb2',
+        'average', 'ta', 'na', 'punishment', 'DC', 'DMC', 'AMC', 'paid', 'owing', 'summonDate', 'summonTime', 'note'
+      ];
+
+
+// Comma-separated list of field names in the INSERT query
+const fieldsList = fields.join(', ');
+// Array of placeholders for the field values in the VALUES part of the INSERT query
+const valuePlaceholders = fields.map(() => '?').join(', ');
+
+const query = `INSERT INTO reportList (${fieldsList}) VALUES (${valuePlaceholders})`;
+
+// Replace this line with the actual values you want to insert into the database
+const reportData = fields.map((field) => {
+    const value = req.body[field];
+    // Convert empty fields to 0
+    return value === "" ? 0 : value;
+  });
+  
+      
+  pool.query(query, reportData, (err, results) => {
+    if (err) {
+      console.error('Error inserting data into the database:', err);
+      res.status(500).json({ error: 'Failed to insert data into the database' });
+    } else {
+      res.json({ message: 'Report created successfully', id: results.insertId });
+    }
+  });
+});
+
+// PUT: Update an existing report by ID
+router.put('/reports/:id', (req, res) => {
+    const pool = req.app.locals.pool;
+    const reportId = req.params.id;
+  
+    const fields = [
+      'term', 'name', 'class', 'DOB', 'POB', 'enrollment', 'master', 'situation', 'test1', 'test2',
+      'eng1', 'fre1', 'math1', 'hist1', 'lit1', 'geo1', 'econs1', 'comm1', 'acc1', 'citi1', 'rel1', 'food1',
+      'chem1', 'bio1', 'phy1', 'comp1', 'sport1', 'logic1', 'hb1',
+      'eng2', 'fre2', 'math2', 'hist2', 'lit2', 'geo2', 'econs2', 'comm2', 'acc2', 'citi2', 'rel2', 'food2',
+      'chem2', 'bio2', 'phy2', 'comp2', 'sport2', 'logic2', 'hb2',
+      'average', 'ta', 'na', 'punishment', 'DC', 'DMC', 'AMC', 'paid', 'owing', 'summonDate', 'summonTime', 'note'
+    ];
+  
+    // Generate the SET clause for the SQL query
+    const setClause = fields.map(field => `${field} = ?`).join(', ');
+  
+    // Create an array of values to be updated in the database
+    const updateValues = fields.map(field => {
+      const value = req.body[field];
+      // Convert empty fields to 0
+      return value === "" ? 0 : value;
+    });
+  
+    // Add the reportId to the end of the updateValues array
+    updateValues.push(reportId);
+  
+    // SQL query to update the report
+    const query = `UPDATE reportList SET ${setClause} WHERE id = ?`;
+  
+    pool.query(query, updateValues, (err, results) => {
+      if (err) {
+        console.error('Error updating data in the database:', err);
+        res.status(500).json({ error: 'Failed to update data in the database' });
+      } else {
+        if (results.affectedRows === 0) {
+          res.status(404).json({ message: 'Report not found' });
+        } else {
+          res.json({ message: 'Report updated successfully' });
+        }
+      }
+    });
+  });
+  
+
+  // DELETE: Delete a report by ID
+router.delete('/reports/:id', (req, res) => {
+    const pool = req.app.locals.pool;
+    const reportId = req.params.id;
+  
+    pool.query('DELETE FROM reportList WHERE id = ?', [reportId], (err, results) => {
+      if (err) {
+        console.error('Error deleting data from the database:', err);
+        res.status(500).json({ error: 'Failed to delete data from the database' });
+      } else {
+        if (results.affectedRows === 0) {
+          res.status(404).json({ message: 'Report not found' });
+        } else {
+          res.json({ message: 'Report deleted successfully' });
+        }
+      }
+    });
+  });
+
+// Add more API endpoints for other CRUD operations as needed
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+const routes = require('./routes');
+const cors = require('cors'); // Import the cors package
+
+const app = express();
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+const dbConfig = {
+  socketPath: '/var/run/mysqld/mysqld.sock',
+  user: 'root',
+  password: '',
+  database: 'report_data'
+};
+
+const pool = mysql.createPool(dbConfig);
+
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Share the pool variable using app.locals
+app.locals.pool = pool;
+
+// Use the routes middleware
+app.use('/api', routes); // You can prefix all your API endpoints with '/api', e.g., '/api/reports'
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started and listening on port ${port}`);
+});
